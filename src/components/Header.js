35c48963d92bac734d106427f98958326b68c95a
caiquemoa <caiquemoa@gmail.com>
2023-02-16 16:08:20 -0300

    header test.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from 'react'
 import './Header.css'
 import facebook from '../assets/facebook.svg'
 import whatsapp from '../assets/whatsapp.svg'
 import tikFoods from '../assets/tikFoods.png'
 
 function Header() {
-  return (
-    <header>
-      <div>
-      <img src={tikFoods} />
-      </div>
-      <div>
-        <img className='icon' src={facebook} />
-        <img className='icon' src={whatsapp} />
-      </div>    
-    </header>
-  );
+	return (
+		<header className="header">
+			<div>
+				<img className="logo" src={tikFoods} />
+			</div>
+			<div className="container">
+				<span className="header-msg">FAÇA JÁ O SEU PEDIDO !</span>
+				<div className="container-icon">
+					<div className="social-midia">
+						<img className="icon" src={whatsapp} />
+						<span>(38) 99892-3984</span>
+					</div>
+					<div className="social-midia">
+						<img className="icon " src={facebook} />
+						<span>Facebook.com/tikFoods</span>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
