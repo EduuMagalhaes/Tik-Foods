b4acb14034effb94c3827f8b5240b196d5f2ccf0
caiquemoa <caiquemoa@gmail.com>
2023-02-16 16:34:42 -0300

    header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,23 @@ function Header() {
 			<div className="container">
 				<span className="header-msg">FAÇA JÁ O SEU PEDIDO !</span>
 				<div className="container-icon">
-					<div className="social-midia">
+					<div
+						className="social-midia"
+						onClick={() => {
+							window.open('http://wa.me/5538998923984')
+						}}
+					>
 						<img className="icon" src={whatsapp} />
 						<span>(38) 99892-3984</span>
 					</div>
-					<div className="social-midia">
+					<div
+						className="social-midia"
+						onClick={() => {
+							window.open('http://facebook.com/sorveteria_e_acai_tik_tok')
+						}}
+					>
 						<img className="icon " src={facebook} />
-						<span>Facebook.com/tikFoods</span>
+						<span>facebook.com/tikFoods</span>
 					</div>
 				</div>
 			</div>
