806fc064bd85245f3563c4fbd5030b6736c5b596
Eduardo <dudu-system@hotmail.com>
2023-02-16 14:41:10 -0300

    config ESLint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+// import OrdersContext from '../context/orders'
 
 function Home() {
-  return(
+  return (
     <div>
       <h1>Home Page</h1>
     </div>
-  )
+  );
 }
 
 export default Home;
