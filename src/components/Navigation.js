664dcd109e29e22e249332db393b858f2e3bc260
caiquemoa <caiquemoa@gmail.com>
2023-02-17 15:55:28 -0300

    navigation style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import './Navigation.css'
 
-function Navigation (){
-  return (
-    <nav>
-      <Link to='/acai'>Açai</Link>
-      <Link to='/cupuacu'>Cupuaçu</Link>
-      <Link to='/macarrao'>Macarrão</Link>
-    </nav>
-  )
+function Navigation() {
+	const { pathname } = useLocation()
+
+	console.log(pathname)
+
+	return (
+		<nav className="nav-bar">
+			<Link
+				className={pathname === '/acai' ? 'page-link -active' : 'page-link'}
+				to="/acai"
+			>
+				Açai
+			</Link>
+			<Link
+				className={pathname === '/cupuacu' ? 'page-link -active' : 'page-link'}
+				to="/cupuacu"
+			>
+				Cupuaçu
+			</Link>
+			<Link
+				className={pathname === '/macarrao' ? 'page-link -active' : 'page-link'}
+				to="/macarrao"
+			>
+				Macarrão
+			</Link>
+		</nav>
+	)
 }
 
-export default Navigation;
+export default Navigation
