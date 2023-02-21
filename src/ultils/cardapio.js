ca885f9c67e013da2747927a75ba977a3301f777
Eduardo <dudu-system@hotmail.com>
2023-02-21 14:14:35 -0300

    ultils-cardapio

---
+++ src/ultils/cardapio.js
@@ -0,0 +1,23 @@
+const acaiIngredients = ['abacaxi', 'amendoim', 'aveia', 'banana', 'bis', 'canudo wafer', 'confete', 'chocolate', 'chantilly', 'coco ralado',
+	'cocada', 'chocoball', 'creme ninho', 'calda de chocolate', 'calda de morango', 'castanha de caju', 'farinha lactea', 'flocos de arroz',
+	'gotas de chocolate', 'granulado', 'granola', 'leite em po', 'leite condensado', 'maca', 'manga', 'marshmallow', 'mel', 'morango', 'negresco',
+	'nutella', 'ovomaltine', 'ouro branco', 'pacoca', 'sorvete', 'sonho de valsa', 'sucrilhos','uva', 'kiwi']
+
+const macarraoIngredients = ['bacon', 'calabresa', 'frango', 'salsicha', 'cenoura', 'cebola', 'pimentao', 'tomate', 'mussarela', 'milho', 'molho especial']
+
+function capitalizerFirstLetter(macarraoIngredients) {
+  return macarraoIngredients.map((word) => word.split(" ")
+    .map((element) => element[0].toUpperCase() + element.slice(1)).join(" "))
+}
+
+function toCamelCase(macarraoIngredients) {
+  return macarraoIngredients.map((word) => word.split(" ")
+    .map((element, index) => index > 0 ? element[0].toUpperCase() + element.slice(1) : element).join(""))
+}
+
+export default {
+  acaiIngredients,
+  macarraoIngredients,
+  capitalizerFirstLetter,
+  toCamelCase
+}
